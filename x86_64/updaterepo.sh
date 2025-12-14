f951d082cb755f7e82507e78d7a9d2f6c1c9a248
Avshalom-Ts <avshalomz.ts@gmail.com>
2025-12-14 23:37:38 +0200

    update

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -3,17 +3,17 @@
 rm forgelinux_repo*
 
 echo "repo-add"
-repo-add -n -R forgelinux_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R forge_linux_repo.db.tar.gz *.pkg.tar.zst
 
 sleep 1
 
-rm forgelinux_repo.db
+rm forge_linux_repo.db
 
-rm forgelinux_repo.files
+rm forge_linux_repo.files
 
-mv forgelinux_repo.db.tar.gz forgelinux_repo.db
+mv forge_linux_repo.db.tar.gz forgelinux_repo.db
 
-mv forgelinux_repo.files.tar.gz forgelinux_repo.files
+mv forge_linux_repo.files.tar.gz forgelinux_repo.files
 
 echo "####################################"
 echo "Repo Updated!!"
